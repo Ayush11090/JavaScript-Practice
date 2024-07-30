3a5717c8a10ed0de8868acc745842835b26afbfc
AB <114931073+Ayush11090@users.noreply.github.com>
2024-07-30 18:33:40 +0000

    array

---
+++ 02_basics.js/01_arrays.js
@@ -0,0 +1,22 @@
+const  myArr = [0, 1, 3, 4, 5, 6]
+
+console.log(myArr[3]);
+
+const myArr2 = ['Shaktiman', 'gangadhar']
+
+myArr2.push(4)
+
+console.log(myArr2);
+myArr.pop()
+
+myArr.unshift(3)
+console.log(myArr);
+
+console.log(myArr.includes(7));
+console.log(myArr.indexOf(1));
+
+
+let newArray = myArr.join()
+
+console.log(myArr);
+console.log(newArray);
