d05e251b76f745db935e688274241737b0bb2b23
AB <114931073+Ayush11090@users.noreply.github.com>
2024-07-30 10:06:39 +0000

    datatypes

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12110600
+let accountEmail = "ayush@gamil.com"
+var accountPassword = "12344"
+accountCity = "Nashik"
+
+// accountId =2   // not allowed
+accountEmail = "ayush11@gmail.com"
+console.table([accountEmail,accountId])
+
+
+// prefer not to use var 
+// because of block scoping
+
+let accountState
+
+console.log(accountState)
---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";  // treat all js as new version
+
+// alert("hello")    // we are using node js not browser
+
+let name = "ayush"
+let age = 67
+let isLoggedIn = true
+
+// number => 2 to power 53
+// BigInt
+// String => " "
+// Boolean => true/false
+// null => standalone val
+// undefined =>
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);       // object
